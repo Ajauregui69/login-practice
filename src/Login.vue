31028d7b356d8850812979fda5c85db2268be5d3
Oswaldo <caballeorblack117@live.com.mx>
2023-02-22 15:51:58 -0600

    Edit Login.vue app file

--- src/Login.vue
+++ src/Login.vue
@@ -1,16 +1,10 @@
-<script setup></script>
+<script setup>
+import { RouterView } from "vue-router";
+</script>
 
 <template>
-  <h1>login view</h1>
-
-  <form action="/action_page.php">
-    <label for="femail">Email:</label><br>
-    <input type="text" id="femail" name="femail" value="John.Smith@yahoo.com"><br>
-    <label for="fpass">Password:</label><br>
-    <input type="text" id="fpass" name="fpass" value="*******"><br><br>
-    <input type="submit" value="Submit">
-  </form>
-
+  <RouterView />
 </template>
 
-<style scoped></style>
+<style scoped>
+</style>
