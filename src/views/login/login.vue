ff8d29a47fb645c86cf6c10318e8437ceaaa2604
Alan Medina <alan.medina1634@gmail.com>
2023-02-22 15:38:25 -0600

    login.vue

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -2,6 +2,15 @@
 
 <template>
   <h1>login view</h1>
+
+  <form action="/action_page.php">
+    <label for="femail">Email:</label><br>
+    <input type="text" id="femail" name="femail" value="John.Smith@yahoo.com"><br>
+    <label for="fpass">Password:</label><br>
+    <input type="text" id="fpass" name="fpass" value="*******"><br><br>
+    <input type="submit" value="Submit">
+  </form>
+
 </template>
 
 <style scoped></style>
