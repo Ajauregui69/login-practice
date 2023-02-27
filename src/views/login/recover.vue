33b60e6839d2eb2950fa4215491ded1c9411ffe6
Oswaldo <caballeorblack117@live.com.mx>
2023-02-27 14:50:12 -0600

    Edited both Recover Password screens

--- src/views/login/recover.vue
+++ src/views/login/recover.vue
@@ -2,15 +2,37 @@
 
 <template>
 
-  <div class="Title">
-    <h1>Recover Password</h1>
-  </div>
+  <div class="flex h-screen w-full relative items-center justify-center">
+    <div class="w-1/5 text-center justify-center">
+      <div class="flex w-550px h-451px" id="recover">
+        <form class="w-sm text-left border-rd-8 w-550px h-451px border-outset border-current">
+          
+          <div class="pl-50px pt-20px pr-50px" id="card">
 
-  <form class="Inputs">
-    <label for="emailInp" class="input-labels">Account Email</label><br>
-    <input type="email" id="emailRecov" name="emailInp"><br>
-    <input type="submit" id="submitButton" value="Next">
-  </form>
+            <div class="text-23px font-700 font-mono"> <!-- Card title -->
+              <h1>Recover</h1>
+            </div>
+
+            <div class="mt-20"> <!-- Inputs div -->
+              <div class="mb-5px font-mono">
+                <label for="Email">Email</label><br>
+              </div>
+              <div>
+                <input type="email" id="email" name="Email" placeholder="Email" class="h-30px w-69 b-rd-5px border-outset border-current font-mono"><br>
+              </div>
+            </div>
+
+            <div class="flex-row mt-100px"> <!-- Buttons div -->
+              <input type="submit" value="Send" class="mr-44px h-40px w-120px bg-#4339F2 text-white b-transparent b-rd-10px font-mono">
+              <input type="submit" value="Login" class="h-40px w-120px b-#4339F2 bg-white text-#4339F2 b-rd-10px font-mono">
+            </div>
+
+          </div>
+
+        </form>
+      </div>
+    </div>
+  </div>
 
 </template>
 
--- src/views/login/recoverPass.vue
+++ src/views/login/recoverPass.vue
@@ -2,17 +2,42 @@
 
 <template>
 
-  <div class="Title">
-    <h1>Recover Password</h1>
-  </div>
+  <div class="flex h-screen w-full relative items-center justify-center">
+    <div class="w-1/5 text-center justify-center">
+      <div class="flex w-550px h-451px" id="recover">
+        <form class="w-sm text-left border-rd-8 w-550px h-451px border-outset border-current">
+          
+          <div class="pl-50px pt-20px pr-50px" id="card">
 
-  <form class="Inputs">
-    <label for="passInp" class="input-labels">New Password</label><br>
-    <input type="password" id="passRecov" name="passInp"><br>
-    <label for="passInp" class="input-labels">Confirm new password</label><br>
-    <input type="password" id="passRecov" name="passInp"><br>
-    <input type="submit" id="submitButton" value="Submit">
-  </form>
+            <div class="text-23px font-700 font-mono"> <!-- Card title -->
+              <h1>Recover Password</h1>
+            </div>
+
+            <div class=""> <!-- Inputs div -->
+              <div class="mb-5px font-mono">
+                <label for="Pass">New Password</label><br>
+              </div>
+              <div>
+                <input type="password" id="pass" name="Pass" placeholder="******" class="h-30px w-69 b-rd-5px border-outset border-current font-mono mb-40px"><br>
+              </div>
+              <div class="font-mono mb-5px">
+                <label for="ConfirmPass">Repeat New Password</label><br>
+              </div>
+              <div>
+                <input type="password" id="confirmPass" name="ConfirmPass" placeholder="******" class="h-30px w-69 b-rd-5px border-outset border-current font-mono"><br>
+              </div>
+            </div>
+
+            <div class="text-center mt-30px"> <!-- Button div -->
+              <input type="submit" value="Send" class="h-50px w-130px bg-#4339F2 text-white b-transparent b-rd-10px font-mono">
+            </div>
+
+          </div>
+
+        </form>
+      </div>
+    </div>
+  </div>
 
 </template>
 
