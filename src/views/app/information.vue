ede9b283885ff4494a33ad25238afd3e924fa4d8
Baruch001 <baruch_001@outlook.es>
2023-03-01 09:00:17 -0600

    Update information.vue
    InfoView

--- src/views/app/information.vue
+++ src/views/app/information.vue
@@ -1,14 +1,13 @@
 <template>
   <div class="flex h-screen w-full items-center justify-center">
     <div class="flex items-center justify-center">
-      <div class="flex h-169px w-936px bg-#4339F2 text-center items-center border-current b-rd rd-10px">  
-        <img src="@/assets/pokeball.png" alt="" sizes="h-156px w-141px" class="p-l-5">
-        <div class="relative p-l-60">
-          <h1 class="text-white text-4xl">Pokemon App</h1>
+      <div class="flex h-169px w-936px bg-#4339F2 text-center items-center b-rd rd-10px">  
+        <img src="@/assets/pokeball.png" alt="" sizes="h-156px w-141px" class="p-l-30px">
+        <div class="relative">
+          <h1 class="text-white h-29px w-145px text-22px m-l-225px m-t-30px">Pokemon App</h1>
           
-          <p class="text-white text-4">we dont know what to do whit this!.</p>
-          
-          <p class="text-white text-4">But to practice with Vue 3</p>
+          <p class="text-white h-40px w-456px text-14px m-l-69px m-b-60px">we dont know what to do whit this!.<br>But to practice with Vue 3</p>
+
         </div>
       </div>
     </div>
