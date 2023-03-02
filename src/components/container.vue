b4c18af49282fcc872155bbefb658d9cdb44f5bc
Alan Medina <alan.medina1634@gmail.com>
2023-03-02 16:13:34 -0600

    Login, input and container

---
+++ src/components/container.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+defineProps({
+    style: 'style',
+    dimentions: 'dimentions'
+})
+
+</script>
+
+<template>
+    <div :style="style" class="flex drop-shadow bg-white shadow-md b-rd-8px">
+        <div :style="dimentions" class="flex">
+            <slot></slot>
+        </div>
+    </div>
+</template>
