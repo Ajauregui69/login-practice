622020bed659dbabac3be1a18a66034d8f030eac
Oswaldo <caballeorblack117@live.com.mx>
2023-03-02 14:53:24 -0600

    Edited Recover Password view

--- src/views/login/recoverPass.vue
+++ src/views/login/recoverPass.vue
@@ -3,35 +3,31 @@
 <template>
 
   <div class="flex h-screen w-full relative items-center justify-center">
-    <div class="w-1/5 text-center justify-center">
-      <div class="flex w-550px h-451px" id="recover">
-        <form class="w-sm text-left border-rd-8 w-550px h-451px border-outset border-current">
-          
-          <div class="pl-50px pt-20px pr-50px" id="card">
+    <div class="flex w-1/5 text-center justify-center">
+      <div class="flex w-550px h-495px" id="recover">
+        <form class="w-sm text-left border-rd-8 pl-63px pt-51px pr-69px w-495px h-550px border-transparent drop-shadow shadow-md">
 
-            <div class="text-23px font-700 font-mono"> <!-- Card title -->
-              <h1>Recover Password</h1>
+          <div class="text-23px font-700 font-sans"> <!-- Card title -->
+            <h1>Change<br>Password</h1>
+          </div>
+
+          <div class=""> <!-- Inputs div -->
+            <div class="mb-8px font-mono mt-40.6px">
+              <label for="Pass">New Password</label><br>
             </div>
-
-            <div class=""> <!-- Inputs div -->
-              <div class="mb-5px font-mono">
-                <label for="Pass">New Password</label><br>
-              </div>
-              <div>
-                <input type="password" id="pass" name="Pass" placeholder="******" class="h-30px w-69 b-rd-5px border-outset border-current font-mono mb-40px"><br>
-              </div>
-              <div class="font-mono mb-5px">
-                <label for="ConfirmPass">Repeat New Password</label><br>
-              </div>
-              <div>
-                <input type="password" id="confirmPass" name="ConfirmPass" placeholder="******" class="h-30px w-69 b-rd-5px border-outset border-current font-mono"><br>
-              </div>
+            <div>
+              <input type="password" id="pass" name="Pass" placeholder="******" class="border-rd-1 h-44px w-369.15px b-1px text-left font-mono b-solid b-#0000001A color-#00000066 px-12px"><br>
             </div>
-
-            <div class="text-center mt-30px"> <!-- Button div -->
-              <input type="submit" value="Send" class="h-50px w-130px bg-#4339F2 text-white b-transparent b-rd-10px font-mono">
+            <div class="font-mono mb-8px mt-57.87px">
+              <label for="ConfirmPass">Repeat New Password</label><br>
             </div>
+            <div>
+              <input type="password" id="confirmPass" name="ConfirmPass" placeholder="******" class="border-rd-1 h-44px w-369.15px b-1px text-left font-mono b-solid b-#0000001A color-#00000066 px-12px"><br>
+            </div>
+          </div>
 
+          <div class="text-center mt-40px"> <!-- Button div -->
+            <input type="submit" value="Send" class="h-62.4px w-146.82px bg-#4339F2 text-white b-transparent b-rd-10px font-mono">
           </div>
 
         </form>
