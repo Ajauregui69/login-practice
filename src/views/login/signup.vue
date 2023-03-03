6d73bfddea752c60748320eb182661301c896a82
Oswaldo <caballeorblack117@live.com.mx>
2023-03-03 12:26:37 -0600

    Add singup view

--- src/views/login/signup.vue
+++ src/views/login/signup.vue
@@ -1,7 +1,31 @@
-<script setup></script>
+<script setup>
+import customCard from '../../components/container.vue';
+import customInput from '../../components/inputs.vue';
+import customBtn from "../../components/button.vue"
+
+</script>
 
 <template>
-  <h1>Sing up</h1>
+  <customCard dimentions="height: 710px; width: 495px;" margins="height: 434px; width: 369.15px; margin: 40px 62.92px">
+
+    <form>
+
+      <h1 class="display-flex display-start color-black text-42px font-bold h-70.45px mb-45.3px">Sign Up</h1>
+
+
+      <customInput type="text" text="Full name" />
+      <customInput type="mail" text="Email" />
+      <customInput type="text" text="Username" />
+      <customInput type="password" text="Password" />
+      <customInput type="password" text="Password" />
+      <customBtn class="mt-57.33px" btnText="Sign up"
+        style="height: 43.87px; width: 146.82px; background-color: #4339F2; border: #4339F2; color: white;">
+
+      </customBtn>
+    </form>
+
+  </customCard>
 </template>
+
 
 <style scoped></style>
