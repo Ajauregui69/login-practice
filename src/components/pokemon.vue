75a4945d7b81401383d29f350e3dd8dc5fb631ec
jose ivan rodriguez rios <kurivan160901@gmail.com>
2023-03-03 10:06:40 -0600

    componente pokemon

---
+++ src/components/pokemon.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { reactive } from 'vue';
+
+defineProps({
+
+    pokemon: {
+        type: Object,
+        required: true,
+    }
+
+})
+
+</script>
+
+<template>
+    <div class="flex content-center bg-#FFFFFF mb-40px h-110px w-1250PX b-rd-10px pl-7px">
+        <td class="font-semibold text-2xl font-mono color-#000000 border-none w-7%"><img alt="Vue logo" class="logo"
+                :src="pokemon.pImage" width="112" height="94" /></td>
+        <div class="flex-none ml-25px mt-31px w-101px">
+            <td class="font-semibold text-22px font-sans color-#000000  border-none w-91px">{{ pokemon.name }}
+            </td>
+            <h4 class="font-sans text-14px opacity-40">pokemon name</h4>
+        </div>
+        <div class="flex-none ml-165px mt-31px">
+            <td class="font-semibold text-22px font-sans color-#000000 w-10%">#{{ pokemon.number }}</td>
+            <h4 class="font-sans text-14px opacity-40">Number</h4>
+        </div>
+        <div class="flex-none ml-219px mt-31px w-60px">
+            <td class="font-semibold text-22px font-sans color-#000000 w-10%">{{ pokemon.ability }}</td>
+            <h4 class="font-sans text-14px opacity-40">Ability</h4>
+        </div>
+        <div class="flex ml-222px mt-50px ">
+            <div :class="pokemon.style" style="align-content: center;">
+                <!--<td class="mt--6px pr-5px"><img :src="pokemon.tImage1" alt="Vue logo" class="logo" width="7.5"
+                        height="15" /></td>-->
+                <img :src="pokemon.tImage1" alt="Vue logo" class="logo ml--3px pt-8px" width="6.5" height="18" />
+                <span class=" font-roboto color-#FFFFFF text-12px ml-6px ">{{ pokemon.type }}</span>
+            </div>
+            <div :class="pokemon.style2">
+                <!--<td class="mt--6px pr-5px"><img :src="pokemon.tImage2" alt="Vue logo" class="logo" width="7.5"
+                        height="15" /></td>-->
+                <img :src="pokemon.tImage2" alt="Vue logo" class="logo ml--1px pt-8px" width="6.5" height="18" />
+                <span class=" font-roboto color-#FFFFFF text-12px mb-5px ml-6px">{{ pokemon.type2 }}</span>
+            </div>
+        </div>
+        <td class="font-semibold text-2xl font-mono color-#000000  mt-45.67px ml-63.83px border-none w--2% opacity-80">
+            <img alt="Vue logo" class="logo" src="@/assets/images/Color.png" width="10" height="16.67" />
+        </td>
+    </div>
+</template>
