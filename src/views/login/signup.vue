0777b50f42111a965485515673cf49a295d480c3
Armando Martín Martínez león <aleon7892@gmail.com>
2023-03-03 12:02:43 -0600

    changes input and signup

--- src/views/login/signup.vue
+++ src/views/login/signup.vue
@@ -1,7 +1,31 @@
-<script setup></script>
+<script setup>
+import customCard from '../../components/container.vue';
+import inputs from '../../components/inputs.vue';
+import customBtn from "../../components/button.vue"
+
+</script>
 
 <template>
-  <h1>Sing up</h1>
+  <customCard dimentions="height: 710px; width: 495px;" margins="height: 434px; width: 369.15px; margin: 40px 62.92px">
+
+    <form>
+
+      <h1 class="display-flex display-start color-black text-42px font-bold h-70.45px mb-45.3px">Sign Up</h1>
+
+
+      <inputs type="text" text="Full name" />
+      <inputs type="mail" text="Email" />
+      <inputs type="text" text="Username" />
+      <inputs type="password" text="Password" />
+      <inputs type="password" text="Password" />
+      <customBtn class="mt-57.33px" btnText="Sign up"
+        style="height: 43.87px; width: 146.82px; background-color: #4339F2; border: #4339F2; color: white;">
+
+      </customBtn>
+    </form>
+
+  </customCard>
 </template>
+
 
 <style scoped></style>
