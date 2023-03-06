51e62fec030b39e7930c4751d3a126a7b52a6842
Oswaldo <caballeorblack117@live.com.mx>
2023-03-06 14:58:23 -0600

    Use components on Recover view

--- src/views/login/recover.vue
+++ src/views/login/recover.vue
@@ -1,36 +1,29 @@
 <script setup>
-
+import customCard from '../../components/container.vue'
+import customBtn from "../../components/button.vue"
+import customInput from '../../components/inputs.vue'
 </script>
 
 <template>
 
-  <div class="flex h-screen w-full relative items-center justify-center">
-    <div class="flex w-1/5 text-center justify-center">
-      <div class="flex w-495px h-451px" id="recover">
-        <form class="w-sm text-left border-rd-8 pl-63px pt-68px pr-62px w-451px h-451px border-transparent drop-shadow shadow-md">
+  <customCard dimentions="height: 451px; width: 495px;" margins="height: 315px; width: 369.15px; margin: 68px 62.85px 68px 63px">
+    <form>
 
-          <div class="text-23px font-700 font-sans"> <!-- Card title -->
-            <h1>Recover</h1>
-          </div>
-
-          <div class="mt-79.25px"> <!-- Inputs div -->
-            <div class="mb-8px font-mono">
-              <label for="Email">Email</label><br>
-            </div>
-            <div>
-              <input type="email" id="email" name="Email" placeholder="Email" class="border-rd-1 h-44px w-369.51px b-1px text-left font-mono b-solid b-#0000001A color-#00000066 px-12px"><br>
-            </div>
-          </div>
-
-          <div class="flex-row mt-81.13px"> <!-- Buttons div -->
-            <input type="submit" value="Send" class="mr-75.51px h-43.87px w-146.82px bg-#4339F2 text-white b-transparent b-rd-10px font-mono">
-            <input type="submit" value="Login" class="h-43.87px w-146.82px b-#4339F2 bg-white text-#4339F2 b-rd-10px font-mono">
-          </div>
-
-        </form>
+      <div class="text-left mb-79.25px"> <!-- Card title -->
+        <h1 class="text-42px font-sans">Recover</h1>
       </div>
-    </div>
-  </div>
+
+      <div class="mb-81.13px"> <!-- Inputs div -->
+        <customInput type="text" text="Email" dimentions="width: 345.15px; height: 20px" />
+      </div>
+
+      <div class="flex justify-between"> <!-- Buttons div -->
+        <customBtn btnText="Send" style="height: 43.87px; width: 146.82px; background-color: #4339F2; border-color: #4339F2; color: white;"/>
+        <customBtn btnText="Login" style="height: 43.87px; width: 146.82px; background-color: white; border-color: #4339F2; color: #4339F2;"/>
+      </div>
+
+    </form>
+  </customCard>
 
 </template>
 
