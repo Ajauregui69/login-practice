2d46731febb6ced446da772bb9ef0f8a8db4cb02
jose ivan rodriguez rios <kurivan160901@gmail.com>
2023-03-06 10:28:17 -0600

    cambio de estilos

--- src/components/inputs.vue
+++ src/components/inputs.vue
@@ -1,5 +1,5 @@
 <!--
-        Custom button component
+        Custom Input component
 ---------------------------------------
 
 This component receives several props to
@@ -18,66 +18,59 @@ import customInput from '../../components/inputs.vue';
 
 -> Change the props into the custom HTML tag
 
-<customInput type="email" text="" />
+<customInput type="text" text="Full name" dimentions="width: 500px; height: 41px" />
 
 / Props */
 
 type - Changed the type of input ( text, email, password...etc.)
 text - Changed the Name of te label and PlaceHolder
+dimentions - changed the dimentions to the input box (the value of the 666 the number of the beast)
+
+
+
 -->
 
 <script setup>
 
 defineProps({
-    type:{
+    type: {
         type: String,
-        default:'text',
+        default: 'text',
         validator: value => ['email', 'text', 'password'].includes(value)
     },
-    text:'text',
+    text: 'text',
+    dimentions: 'dimentons'
 
 })
 
 </script>
 
 <template>
-    <div class="flex-column h-66px w-369.15px br-5px">
-        <label class="text-start mb-8px mt-11px" >{{ text }}</label>
-        <input id="div2" :type="type" name="input" :placeholder="text">
+    <div class="flex-column h-66px w-369.15px br-5px mb-11px mt-11px">
+        <label class="text-start mb-8px mt-0 font-bold font-400 ">{{ text }}</label>
+        <input :type="type" :placeholder="text" :style="dimentions">
     </div>
-
-
-
 </template>
 
 
 <style scoped>
+label {
+    width: 94px;
+    height: 14px;
+    font-family: 'Roboto';
+    font-size: 12px;
+    line-height: 14px;
+    display: flex;
+    color: #000000;
+}
 
-    label{
-        width: 94px;
-        height: 14px;
-
-        font-family: 'Roboto';
-        font-style: normal;
-        font-weight: 400;
-        font-size: 12px;
-        line-height: 14px;
-        display: flex;
-        color: #000000;
-    }
-    input {
-        display: flex;
-        flex-direction: row;
-        align-items: flex-start;
-        padding: 12px;
-        gap: 16px;
-
-        width: 369.15px;
-        height: 44px;
-
-        background: #FFFFFF;
-        border: 1px solid rgba(0, 0, 0, 0.1);
-        box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.05);
-        border-radius: 5px;
-    }
+input {
+    display: flex;
+    width: 373.1px;
+    height: 41px;
+    background: #FFFFFF;
+    border: 1px solid rgba(0, 0, 0, 0.1);
+    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.05);
+    border-radius: 5px;
+}
 </style>
