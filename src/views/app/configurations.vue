7c96bdbc1b599901c98964ea9a4d84b50ed2895f
diegoavalosf <diego_avalos@live.com.mx>
2023-03-13 11:03:10 -0600

    UpdateConfig

--- src/views/app/configurations.vue
+++ src/views/app/configurations.vue
@@ -1,41 +1,31 @@
 
 <script setup>
     import customBtn from "../../components/button.vue";
-    import customCard from '../../components/container.vue';
     import customInput from '../../components/inputs.vue';
 </script>
 
 
 <template>
 
-<customCard dimentions="height: auto; width: calc(100% - 200px);" margins="height: auto; width: calc(100% - 120px); margin: 40px 62.92px">
-
-    <form class="flex-column w-100%" action="" >
+    <form class="flex-column w-1000px" action="" >
         <div class="mb-15px">
             <h1 class="display-flex font-bold text-42px font-inter">Update User</h1> 
         </div>
 
-        <div class="mb-15px">
-            <!-- <label class="text-center font-roboto text-12px font-semibold" for="username">Username</label><br>
-            <input class="mt-8px border-rd-1 h-44px w-616px text-16px b-1px text-left font-roboto b-solid b-#0000001A color-#00000066 p-12px shadow" type="text" id="username" name="name" value="chuckylozano"><br> -->
+        <div class="mb-15px">       
             <customInput type="text" text="Username" dimentions="width: 480px; height: 20px"/>
         </div>
         
-        <div class="mb-15px">
-            <!-- <label class="text-center font-roboto text-12px font-semibold" for="email">Email</label><br>
-            <input class="mt-8px border-rd-1 h-44px w-616px text-16px b-1px text-left font-roboto b-solid b-#0000001A color-#00000066 p-12px shadow" type="email" id="email" name="email" value="ejemplo@ex.ex"><br> -->
+        <div class="mb-15px">          
             <customInput type="email" text="Email" dimentions="width: 480px; height: 20px"/>
         </div>
 
-        <div class="mb-15px">
-            <!-- <label class="text-center font-roboto text-12px font-semibold" for="email">Full Name</label><br>
-            <input class="mt-8px border-rd-1 h-44px w-616px text-16px b-1px text-left font-roboto b-solid b-#0000001A color-#00000066 p-12px shadow" type="text" id="fullname" name="fullname" value="El Chucky Lozano"><br> -->
+        <div class="mb-15px">          
             <customInput type="text" text="Full Name" dimentions="width: 480px; height: 20px"/>
         </div>
 
         <div class="mb-30px">
-            <!-- <label class="text-center font-roboto text-12px font-semibold" for="email">Password</label><br>
-            <input class="mt-8px border-rd-1 h-44px w-616px text-16px b-1px text-left font-roboto b-solid b-#0000001A color-#00000066 p-12px shadow" type="password" id="password" name="password" value="000000"><br> -->
+           
             <customInput type="password" text="Password" dimentions="width: 480px; height: 20px"/>
         </div>
         
@@ -44,9 +34,6 @@
         </div>
                 
     </form>
-
-
-</customCard>
 
 </template>
 
