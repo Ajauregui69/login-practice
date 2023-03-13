624fded69984f6bffbd57f78b001301efc64edb7
diegoavalosf <diego_avalos@live.com.mx>
2023-03-13 16:12:42 -0600

    update3

--- src/views/app/configurations.vue
+++ src/views/app/configurations.vue
@@ -6,33 +6,36 @@
 
 <template>
 
-    <form class="flex-column w-1000px" action="" >
-        <div class="mb-15px">
-            <h1 class="display-flex font-bold text-42px font-inter">Update User</h1> 
-        </div>
+<div class="bg-#F8F8F8 flex h-screen w-screen justify-center items-center">
+    <div class="drop-shadow bg-white shadow-md b-rd-8px px-60px py-40px my-width">
 
-            <div class="mb-15px">       
-                <customInput type="text" text="Username" dimentions="width: 480px; height: 20px"/>
-            </div>
-            
-            <div class="mb-15px">          
-                <customInput type="email" text="Email" dimentions="width: 480px; height: 20px"/>
+        <form class="flex-column w-1000px" action="" >
+            <div class="mb-15px">
+                <h1 class="display-flex font-bold text-42px font-inter">Update User</h1> 
             </div>
 
-            <div class="mb-15px">          
-                <customInput type="text" text="Full Name" dimentions="width: 480px; height: 20px"/>
-            </div>
+                <div class="mb-15px">       
+                    <customInput type="text" text="Username" dimentions="width: 480px; height: 20px"/>
+                </div>
+                
+                <div class="mb-15px">          
+                    <customInput type="email" text="Email" dimentions="width: 480px; height: 20px"/>
+                </div>
 
-            <div class="mb-30px">
-            
-                <customInput type="password" text="Password" dimentions="width: 480px; height: 20px"/>
-            </div>
-            
-            <div class="text-center ">
-                <customBtn btnText="Update" style="height: 50px; width: 400px; background-color: #4339F2; border: #4339F2; color: white;"/>
-            </div>
-                    
-        </form>
+                <div class="mb-15px">          
+                    <customInput type="text" text="Full Name" dimentions="width: 480px; height: 20px"/>
+                </div>
+
+                <div class="mb-30px">
+                
+                    <customInput type="password" text="Password" dimentions="width: 480px; height: 20px"/>
+                </div>
+                
+                <div class="text-center ">
+                    <customBtn btnText="Update" style="height: 50px; width: 400px; background-color: #4339F2; border: #4339F2; color: white;"/>
+                </div>
+                        
+            </form>
     </div>
 </div>
 
